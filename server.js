d1d190ffbc51ae09de217658afaf597a801ff9a6
Harsh Patel <hpatel371@myseneca.ca>
2023-11-06 22:06:49 -0500

    Assignment 4

--- server.js
+++ server.js
@@ -8,18 +8,21 @@
 *
 * Name: Harsh Patel Student ID: 114085228 Date: 10/27/2023
 *
-* Published URL: 
+* Published URL:
 *
 ********************************************************************************/
-
 const express = require('express');
 const path = require('path');
-
 const app = express();
-const legoData = require('./modules/legoSets');
-const PORT = 3000;
 
-// Serve static files from the 'public' directory
+
+const legoData = require('./modules/legoSets'); 
+
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs"); 
+app.set("views", path.join(__dirname, "views")); 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(async (req, res, next) => {
@@ -36,12 +39,14 @@ app.use(async (req, res, next) => {
 
 // Home route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
+    res.render('home', { page: '/' }); 
 });
+
+
 
 // About route
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/about.html'));
+    res.render('about' , { page: '/' });
 });
 
 // Route for all LEGO sets
@@ -53,34 +58,36 @@ app.get('/lego/sets', async (req, res) => {
         } else {
             sets = await legoData.getAllSets(app.locals.sets);
         }
-        res.json(sets);
+        res.render('sets', { sets, page: '/lego/sets' });
     } catch (error) {
-        res.status(404).send(error.toString());
+        res.status(404).render('404', { message: error.toString() });
     }
 });
+
 
 // Route for a specific LEGO set by number
-app.get('/lego/sets/:set_num', async (req, res) => {
+app.get('/lego/sets/:num', async (req, res) => {
+    const setNum = req.params.num;
+
     try {
-        const set = await legoData.getSetByNum(app.locals.sets, req.params.set_num);
-        res.json(set);
+        const legoSet = await legoData.getSetByNum(app.locals.sets, setNum);
+        if (legoSet) {
+            res.render("set", { set: legoSet });
+        } else {
+            res.status(404).render('404', { message: "LEGO set not found" });
+        }
     } catch (error) {
-        res.status(404).send(error.toString());
+        res.status(500).render('500', { message: "Error retrieving LEGO set data" });
     }
 });
 
-// Custom 404 error page
+
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+    res.status(404).render('404', { page: req.url, message: "I'm sorry, we're unable to find what you're looking for" });
 });
 
-// Start the server
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
-
-
-
-
