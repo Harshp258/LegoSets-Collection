639dc2bd24ae14a60c38d5e2566ee525f8be0094
Harsh Patel <hpatel371@myseneca.ca>
2023-11-10 17:42:50 -0500

    s

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ const legoData = require('./modules/legoSets');
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs"); 
-app.set("views", path.join(__dirname, "views")); 
-
 app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"));
+
 
 app.use(async (req, res, next) => {
     try {
